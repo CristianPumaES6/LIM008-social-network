650a79bb3b96a5915819ccc67b3157db0bcf804d
cristianPumaES6 <cristian.puma.es6@gmail.com>
2019-02-05 17:43:57 -0500

    agregue la funcion de authFAcebook y authGoogle

---
+++ src/lib/authBD/authFirebase.js
@@ -0,0 +1,10 @@
+let provider;
+export const authenticateFacebook = () => {
+    provider = new firebase.auth.FacebookAuthProvider();
+    return firebase.auth().signInWithPopup(provider);
+};
+
+export const authenticateGoogle = () => {
+    provider = new firebase.auth.GoogleAuthProvider();
+    return firebase.auth().signInWithPopup(provider);
+};
