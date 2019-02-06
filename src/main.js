f15bd33ebd9f30530e4a97ae0801edaf72e7de58
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-02-05 16:20:16 -0800

    se importo la funcion initRouter de UI/router.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 // Este es el punto de entrada de tu aplicacion
 import { configBD } from './config/configFireBase.js';
+import { initRouter } from './UI/router.js';
 
 window.onload = () => {
+	
 	configBD();
+	initRouter();
 }
