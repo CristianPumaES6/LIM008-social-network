66faae836b5f0c534e3b9eab1fda3e8d3af52ee8
Mayra AlexandraTito Casavilca <mayrat.casavilca@gmail.com>
2019-02-06 12:13:52 -0500

    se importo Register de templates/pagRegister.js

--- src/UI/router.js
+++ src/UI/router.js
@@ -1,7 +1,7 @@
-import Inite from './templates/pagInite.js';
-
-
+import Inite from './templates/pagInit.js';
+import Register from './templates/pagRegister.js';
 
 export const initRouter = () => {
     Inite();
+    Register();
 };
