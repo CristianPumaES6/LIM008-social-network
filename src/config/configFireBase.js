9107a822eea5cc1728523178a58ac48825752059
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-02-05 20:24:10 -0800

    Correcion config.

--- src/config/configFireBase.js
+++ src/config/configFireBase.js
@@ -1,12 +1,13 @@
 export const configBD = () => {
-  const config ={
+  const config = {
     apiKey: "AIzaSyAmHyokWpQDVsmekrrcRfVkWI4sHkttmsM",
     authDomain: "networking-43ee6.firebaseapp.com",
     databaseURL: "https://networking-43ee6.firebaseio.com",
     projectId: "networking-43ee6",
     storageBucket: "networking-43ee6.appspot.com",
     messagingSenderId: "146347443700"
-  };;
+  };
   firebase.initializeApp(config);
+ 
   return 1;
 }; 
