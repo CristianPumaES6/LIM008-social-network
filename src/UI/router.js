0477964de4e51a1d48731cb9e74b101c193909c9
Mayra AlexandraTito Casavilca <mayrat.casavilca@gmail.com>
2019-02-06 17:18:16 -0500

    se importo una funcion Home() de templates/home.js

--- src/UI/router.js
+++ src/UI/router.js
@@ -1,9 +1,11 @@
 import Inite from './templates/pagInit.js';
 import Register from './templates/pagRegister.js';
 import Login from './templates/pagLogin.js';
+import Home from './templates/home.js';
 
 export const initRouter = () => {
     Inite();
     Register();
     Login();
+    Home();
 };
