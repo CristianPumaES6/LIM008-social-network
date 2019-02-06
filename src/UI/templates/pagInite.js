663b922079af492f293aae8bf0401c5a657928df
Mayra AlexandraTito Casavilca <mayrat.casavilca@gmail.com>
2019-02-06 12:15:38 -0500

    se elimino

--- src/UI/templates/pagInite.js
+++
@@ -1,9 +0,0 @@
-import { accesWithFbOrGoogle } from '../view-controller.js';
-
-export default () => {
-    // Login Facebook and Google
-    const buttonFacebook = document.getElementById('buttonFacebook');
-    const buttonGoogle = document.getElementById('buttonGoogle');
-
-    accesWithFbOrGoogle(buttonFacebook, buttonGoogle);
-}
