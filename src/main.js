e6c3dfd4ee9f0b2085a639e91e9e48c4efad29c9
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-02-07 12:15:30 -0800

    agrego event al btnFile con una funcion instanciando firestore

--- src/main.js
+++ src/main.js
@@ -3,7 +3,25 @@ import { configBD } from './config/configFireBase.js';
 import { initRouter } from './UI/router.js';
 
 window.onload = () => {
-	
+
 	configBD();
 	initRouter();
+
+	var fichero = document.getElementById("fichero");
+
+	fichero.addEventListener("change", () => {
+
+		firebase.firestore().collection('IMG').doc("123").set({
+			"nombre": fichero.files[0].name,
+			"url": "www.google.com"
+		}, { merge: true }).then(function (result) {
+			console.log("----------------------CORRECTO")
+			console.log(result)
+		}).catch(function (err) {
+			console.log("----------------------ERROR")
+			console.log(err)
+		})
+
+	}, false);
+
 }
