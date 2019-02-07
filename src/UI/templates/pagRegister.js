7904b9a8db1954fa8d84a77be613f038312de749
Mayra AlexandraTito Casavilca <mayrat.casavilca@gmail.com>
2019-02-07 01:32:13 -0500

    se agrego template de pagRegister

--- src/UI/templates/pagRegister.js
+++ src/UI/templates/pagRegister.js
@@ -1,11 +1,24 @@
-import { btnAcceptRegisterAndSendToHome} from '../view-controller.js';
+import { btnAcceptRegisterAndSendToHome } from '../view-controller.js';
 
 export default () => {
-    
-  const createUsername = document.getElementById('createUsername'); 
+
+  const container = document.getElementById('container');
+  const tPagRegister = `
+  <h2 class="text-center">únete a nuestra comunidad</h2>
+  <form>
+    <input type="text" id="createUsername" class="username" placeholder="nombre de usuario">
+    <input type="text" id="createEmail"  class="correo" placeholder="correo">
+    <input type="password" id="createPassword" class="password" placeholder="constraseña">
+    <button class="btn-register" id="buttonAcceptRegister">
+    Registrar
+    </button>
+  </form>
+  `;
+  container.innerHTML = tPagRegister;
+  const createUsername = document.getElementById('createUsername');
   const createEmail = document.getElementById('createEmail');
   const createPassword = document.getElementById('createPassword');
   const buttonAcceptRegister = document.getElementById('buttonAcceptRegister');
-  
+
   btnAcceptRegisterAndSendToHome(createUsername, createEmail, createPassword, buttonAcceptRegister);
 }
