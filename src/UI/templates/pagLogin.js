c7c09e962493082f64a1479466abe2cb3d878cbe
Mayra AlexandraTito Casavilca <mayrat.casavilca@gmail.com>
2019-02-07 01:31:50 -0500

    se agrego template de pagLogin

--- src/UI/templates/pagLogin.js
+++ src/UI/templates/pagLogin.js
@@ -1,7 +1,17 @@
 import { btnAcceptLoginAndSendToHome } from '../view-controller.js';
 
 export default () => {
-
+    const container = document.getElementById('container');
+    const tPagRegister = `
+    
+      <h2 class="text-center">communitytech</h2>
+      <input type="text" id="inputEmail" class="username-o-correo" placeholder="nombre de usuario o correo">
+      <input type="password" id="inputPassword" class="password-enter" placeholder="contraseña">
+      <h5 class="missed">¿Olvidaste tu cuenta?</h7>
+      <button id="buttonAcceptLogin" class="btn-inite-sesion">Iniciar sesion</button>
+    
+    `
+    container.innerHTML = tPagRegister;
     const inputEmail = document.getElementById('inputEmail');
     const inputPassword = document.getElementById('inputPassword');
     const buttonAcceptLogin = document.getElementById('buttonAcceptLogin');
