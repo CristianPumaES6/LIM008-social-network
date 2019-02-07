8556cbbfbb50d204ca5ae27be1948f047fb2a3fe
Mayra AlexandraTito Casavilca <mayrat.casavilca@gmail.com>
2019-02-07 01:29:44 -0500

    se creo una funcion de enrutamiendo hash y usamos switch para tmp

--- src/UI/router.js
+++ src/UI/router.js
@@ -3,9 +3,58 @@ import Register from './templates/pagRegister.js';
 import Login from './templates/pagLogin.js';
 import Home from './templates/home.js';
 
+const changeTmp = (hash) => {
+    console.log("Invocar changeTmp = " + hash);
+
+    if (hash === '#/' || hash === '' || hash === '#') {
+        return viewTmp('#/init');
+    } else if (hash === '#/pagInitSession' || hash === '#/pagRegister' || hash === '#/home') {
+        return viewTmp(hash);
+    };
+}
+
+const viewTmp = (routers) => {
+    console.log("invocacion funcion viewTpm = " + routers)
+    const router = routers;
+
+    const container = document.getElementById('container');
+    console.log("\n datos del HTML \n" + container.innerHTML);
+    container.innerHTML = ' ';
+
+    switch (router) {
+        case '#/init':
+            Inite();
+            break;
+        case '#/pagInitSession':
+            Login();
+            break;
+        case '#/pagRegister':
+            Register();
+            break;
+        case '#/home':
+            Home();
+            break;
+        default:
+            // Inite();
+            break;
+    }
+
+
+    
+
+
+}
+
 export const initRouter = () => {
-    Inite();
-    Register();
-    Login();
-    Home();
-};
+
+    window.addEventListener('load', changeTmp(window.location.hash));
+
+    if ('onhashchange' in window) {
+        window.onhashchange = () => {
+            changeTmp(window.location.hash)
+        }
+    }
+
+}
+
+
