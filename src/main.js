1f6808b7c172570c1e228aba7afd5a26f64baa21
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-02-07 19:49:46 -0800

    PostBYIdUser

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 import { configBD } from './config/configFireBase.js';
 import { initRouter } from './UI/router.js';
 
+//ESTO SE SEBE DE BORRAR.
+import { dataConnectUser } from './lib/authBD/authFirebase.js'
 window.onload = () => {
 
 	configBD();
@@ -11,16 +13,32 @@ window.onload = () => {
 
 	fichero.addEventListener("change", () => {
 
-		firebase.firestore().collection('IMG').doc("123").set({
-			"nombre": fichero.files[0].name,
-			"url": "www.google.com"
-		}, { merge: true }).then(function (result) {
-			console.log("----------------------CORRECTO")
-			console.log(result)
-		}).catch(function (err) {
-			console.log("----------------------ERROR")
-			console.log(err)
-		})
+		let imageASubir = fichero.files[0];
+		var uploadTask = firebase.storage().ref().child('images/' + imageASubir.name).put(imageASubir).then((snapshot) => {
+			snapshot.ref.getDownloadURL().then((getURL) => {
+
+
+				firebase.firestore().collection('Post').add({
+					"idUser": dataConnectUser().email,
+					"tittle": "redes Inalambricas",
+					"urlImage": getURL,
+					"description": "las resdes inalambricas estan en todas partes del mundo."
+				}).then(function (result) {
+					console.log("----------------------CORRECTO")
+					console.log(result)
+				}).catch(function (err) {
+					console.log("----------------------ERROR firebase.firestore().collection() linea 26 mainJS")
+					console.log(err)
+				});
+
+
+			})
+		}).catch((err) => {
+			alert(err)
+		});
+
+
+		/*  */
 
 	}, false);
 
