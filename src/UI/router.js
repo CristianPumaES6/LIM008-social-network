e9249d3895a2406a7c6b1a8dcbe7ecf7a20ec57a
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-02-14 12:21:47 -0500

    routerJS

--- src/UI/router.js
+++ src/UI/router.js
@@ -1,60 +1,43 @@
-import Inite from './templates/pagInit.js';
-import Register from './templates/pagRegister.js';
+import Inite from './templates/pagInite.js';
 import Login from './templates/pagLogin.js';
+import Register from './templates/pagRegister.js';
 import Home from './templates/home.js';
+import Post from './templates/post.js';
+
+import {editCreatePost} from './view-controller.js';
 
 const changeTmp = (hash) => {
-    console.log("Invocar changeTmp = " + hash);
-
-    if (hash === '#/' || hash === '' || hash === '#') {
-        return viewTmp('#/init');
-    } else if (hash === '#/pagInitSession' || hash === '#/pagRegister' || hash === '#/home') {
-        return viewTmp(hash);
-    };
-}
+  if (hash === '#/' || hash === '' || hash === '#') {
+    return viewTmp('#/inite');
+  } else if (hash === '#/pagIniteSesion' || hash === '#/pagRegister' || hash === '#/home' || hash === '#/createPost' || hash === '#/updatePost') {
+    return viewTmp(hash);
+  } else {
+    return viewTmp('#/inite');
+  }
+};
 
 const viewTmp = (routers) => {
-    console.log("invocacion funcion viewTpm = " + routers)
-    const router = routers;
-
-    const container = document.getElementById('container');
-    console.log("\n datos del HTML \n" + container.innerHTML);
-    container.innerHTML = ' ';
-
-    switch (router) {
-        case '#/init':
-            Inite();
-            break;
-        case '#/pagInitSession':
-            Login();
-            break;
-        case '#/pagRegister':
-            Register();
-            break;
-        case '#/home':
-            Home();
-            break;
-        default:
-            // Inite();
-            break;
-    }
-
-
-    
-
-
-}
+  const router = routers.substr(2, routers.length - 2);
+  const container = document.getElementById('container');
+  container.innerHTML = '';
+  switch (router) {
+  case 'inite':
+    Inite(); break;
+  case 'pagIniteSesion':
+    Login(); break;
+  case 'pagRegister':
+    Register(); break;
+  case 'home':
+    Home(); break;
+  case 'createPost':
+    // UpdatePost(userName, userPhoto, titlePost, multimedia, descriptionPost);
+    Post(editCreatePost); break;
+  default:
+    Inite(); break;
+  }
+};
 
 export const initRouter = () => {
-
-    window.addEventListener('load', changeTmp(window.location.hash));
-
-    if ('onhashchange' in window) {
-        window.onhashchange = () => {
-            changeTmp(window.location.hash)
-        }
-    }
-
-}
-
-
+  window.addEventListener('load', changeTmp(window.location.hash));
+  if (('onhashchange' in window)) window.onhashchange = () => changeTmp(window.location.hash);
+};
