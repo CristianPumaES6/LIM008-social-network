456aaebb562ae66672bc3c7964addd2cf3225a5f
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-02-14 12:31:51 -0500

    Config. Firebase.

--- src/lib/authBD/authFirebase.js
+++ src/lib/authBD/authFirebase.js
@@ -1,35 +1,38 @@
+export const createUser = (email, password) => {
+  return firebase.auth().createUserWithEmailAndPassword(email, password);
+};
 
-let provider;
+export const logInUser = (email, password) => {
+  return firebase.auth().signInWithEmailAndPassword(email, password);
+};
+
 export const authenticateFacebook = () => {
-    provider = new firebase.auth.FacebookAuthProvider();
-    return firebase.auth().signInWithPopup(provider);
+  let provider = new firebase.auth.FacebookAuthProvider(); 
+  return firebase.auth().signInWithPopup(provider);
 };
 
 export const authenticateGoogle = () => {
-    provider = new firebase.auth.GoogleAuthProvider();
-    return firebase.auth().signInWithPopup(provider);
-};
-
-
-
-export const createUser = (email, password) => {
-    return firebase.auth().createUserWithEmailAndPassword(email, password);
-};
-
-
-export const logInUser = (email, password) => {
-    return firebase.auth().signInWithEmailAndPassword(email, password);
-};
-
-export const userStateChange = (user) => {
-
-    return firebase.auth().onAuthStateChanged((userState) => user.push(userState));;
+  let provider = new firebase.auth.GoogleAuthProvider(); 
+  return firebase.auth().signInWithPopup(provider);
 };
 
 export const logOutUser = () => {
-    return firebase.auth().signOut();
+  return firebase.auth().signOut();
+};
+
+export const sendEmail = (config) => {
+  return firebase.auth().currentUser.email.sendEmailVerification(config);
+};
+
+export const passwordReset = (email) => {
+  return firebase.auth().sendPasswordResetEmail(email);
+};
+
+export const userStateChange = (user) => {
+  firebase.auth().onAuthStateChanged((userState) => user.push(userState));
+  return user;
 }
 
 export const dataConnectUser = () => {
-    return firebase.auth().currentUser;
+  return firebase.auth().currentUser;
 }
