0f4f6003dd33f71966b00f98302d9a4e8c0f58fc
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-02-14 12:33:46 -0500

    test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,11 +1,10 @@
 
-// configurando firebase mock
+// configurando firebase moc
 const firebasemock = require('firebase-mock');
 const mockauth = new firebasemock.MockFirebase();
 const mockfirestore = new firebasemock.MockFirestore();
 mockfirestore.autoFlush();
 mockauth.autoFlush();
-
 
 global.firebase = firebasemock.MockFirebaseSdk(
   // use null if your code does not use RTDB
@@ -14,83 +13,61 @@ global.firebase = firebasemock.MockFirebaseSdk(
   () => mockfirestore
 );
 
+// iniciando test
 
-// iniciando tests
+import { createUser, logInUser, authenticateFacebook, authenticateGoogle, passwordReset,logOutUser } from "../src/lib/authBD/authFireBase.js"
 
-import { createUser, logInUser, authenticateGoogle, authenticateFacebook, logOutUser, } from "../src/lib/authBD/authFirebase.js";
-
-
-describe('createUser', () => {
-
-  it('Deberia ser una funcion.', () => {
+describe('createUser', () =>{
+  it('Deberia ser una funcion', () => {
     expect(typeof (createUser)).toBe('function');
   });
-
-  it('Debería poder crear un nuevo usuario.', () => {
-    return createUser('mayratc@gmail.com', '123456').then((result) => {
-      expect(result.email).toBe('mayratc@gmail.com')
-    });
-  });
-
-});
-
-
-describe('logInUser', () => {
-
-  it('Deberia ser una funcion.', () => {
-    expect(typeof (logInUser)).toBe('function');
-  });
-
-  it('Debería poder iniciar sesion.', () => {
-    return logInUser('mayratc@gmail.com', '123456')
-      .then((user) => {
-        expect(user.email).toBe('mayratc@gmail.com')
+  it('Deberia poder crear un nuevo usuario', () => {
+    return createUser('mayrat.casavilca@gmail.com', '123456')
+      .then((result) => {
+        expect(result.email).toBe('mayrat.casavilca@gmail.com')
       });
   });
-
 });
 
+describe('logInUser', () => {
+  it('Deberia ser una funcion', () => {
+    expect(typeof (logInUser)).toBe('function');
+  });
+  it('Deberia poder iniciar sesion', () => {
+    return logInUser('mayrat.casavilca@gmail', '123456')
+      .then((user) => {
+        expect(user.email).toBe('mayrat.casavilca@gmail')
+      });
+  });
+});
+
+describe('authenticateFacebook', () => {
+  it('Deberia ser una funcion', () => {
+    expect(typeof (authenticateFacebook)).toBe('function');
+  });
+  it('Deberia poder iniciar sesion con Facebook', () => {
+    return authenticateFacebook().then((result) => {
+      expect(typeof result).toBe('object');
+    });
+  });
+});
 
 describe('authenticateGoogle', () => {
-
-  it('Deberia ser una funcion.', () => {
+  it('Deberia ser una funcion', () => {
     expect(typeof (authenticateGoogle)).toBe('function');
   });
-
-  it('Debería poder autenticarse con Google.', () => {
+  it('Deberia poder inisiar sesion con Google', () => {
     return authenticateGoogle().then((result) => {
       expect(typeof result).toBe('object')
     });
   });
-
 });
-
-
-describe('authenticateFacebook', () => {
-
-  it('Deberia ser una funcion.', () => {
-    expect(typeof (authenticateFacebook)).toBe('function');
-  });
-
-  it('Debería poder autenticarse con facebook.', () => {
-    return authenticateFacebook().then((result) => {
-      expect(typeof result).toBe('object')
-    });
-  });
-
-});
-
 
 describe('logOutUser', () => {
-
-  it('Deberia ser una funcion.', () => {
+  it('Deberia ser una funcion', () => {
     expect(typeof (logOutUser)).toBe('function');
   });
-
-  it('Debería poder cerrar sesion.', () => {
-    return logOutUser().then((result) => {
-      expect(result).toBe(undefined)
-    });
+  it('Deberia poder cerrar sesion', () => {
+    return logOutUser();
   });
-
 });
